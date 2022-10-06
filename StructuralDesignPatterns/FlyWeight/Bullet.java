2a7459277ff92cd4ef3e023e9230f07cf3b3766a
Srikanth Dumpeti <d.sri6365@gmail.com>
2022-10-07 02:02:09 +0530

    Flyweight Design Pattern

---
+++ StructuralDesignPatterns/FlyWeight/Bullet.java
@@ -0,0 +1,10 @@
+package StructuralDesignPatterns.FlyWeight;
+
+public class Bullet {
+    String type;
+    double weight;
+    String color;
+    String image;
+    double maxPower;
+
+}
---
+++ StructuralDesignPatterns/FlyWeight/FlyingBullet.java
@@ -0,0 +1,12 @@
+package StructuralDesignPatterns.FlyWeight;
+
+public class FlyingBullet {
+    double xCordinate;
+    double yCordinate;
+    double zCordinate;
+
+    double speed;
+    String direction;
+
+    Bullet bullet;    //Adding the "Bullet" ==> Attribute in Flying Bullet.
+}
