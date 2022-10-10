46aaeb91d375cdec8fedfcc8eb1f96bd10c7cdac
Srikanth Dumpeti <d.sri6365@gmail.com>
2022-10-11 00:23:13 +0530

    checking if array is sorted.

--- DSA solve/Recursion/src/IsSortedArray.java
+++ DSA solve/Recursion/src/IsSortedArray.java
@@ -1,7 +1,7 @@
 public class IsSortedArray {
 
     public static void main(String[] args) {
-        int[] a = {10,2,3,4,5};
+        int[] a = {1,2,3,4,5};
         int idx = 0;
         int n = a.length;
         System.out.println(isSorted(a, idx, n));
@@ -10,9 +10,14 @@ public class IsSortedArray {
         if(idx == n-1){
             return true;
         }
+        /*
+        //We can do it the below way also.
+
         if(a[idx] > a[idx + 1]){
             return false;
         }
-        return isSorted(a, idx+1, n);
+        return isSorted(a, idx+1, n);*/
+
+        return a[idx] < a[idx + 1] && isSorted(a, idx + 1 , n);
     }
 }
