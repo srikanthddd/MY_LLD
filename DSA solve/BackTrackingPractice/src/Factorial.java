04b6ae507000ede49eb51bc580fa187a1d5ecaa5
Srikanth Dumpeti <d.sri6365@gmail.com>
2022-10-10 12:41:05 +0530

    Factorial recursion

---
+++ DSA solve/BackTrackingPractice/src/Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorial(n));
+    }
+    public static int factorial(int n){
+        if(n==1) return 1;
+        return n * factorial(n-1);
+    }
+}
