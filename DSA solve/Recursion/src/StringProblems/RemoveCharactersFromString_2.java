c5dd64d6a08d4d275cc87520cf99b2e76b487f21
Srikanth Dumpeti <d.sri6365@gmail.com>
2022-10-11 01:24:52 +0530

    Remove given characters from the given string. (Another approach)

---
+++ DSA solve/Recursion/src/StringProblems/RemoveCharactersFromString_2.java
@@ -0,0 +1,28 @@
+package StringProblems;
+
+public class RemoveCharactersFromString_2 {
+
+    public static void main(String[] args) {
+        String a = "baccad";
+        char charToRemove = 'a';
+        String out = "";
+        int idx = 0;
+        System.out.println(removeCharacters(a, idx, charToRemove));
+    }
+
+    //In this approach, we are not taking the output string in the argument, Instead, for every function call we are taking it new one.
+    // And adding the result from the below function calls to the current one.
+    public static String removeCharacters(String a, int idx, char target){
+        String currOut = "";
+        if(idx == a.length()){
+            return  currOut;
+        }
+        if(a.charAt(idx)==target){
+            return removeCharacters(a, idx + 1 , target);
+        }
+        currOut = currOut + a.charAt(idx);
+        String outFromBelowFunctions  =  removeCharacters(a, idx + 1 , target);;
+        return currOut + outFromBelowFunctions;
+    }
+}
+
