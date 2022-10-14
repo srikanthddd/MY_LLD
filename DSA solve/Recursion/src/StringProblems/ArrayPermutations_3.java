c5e75cb86875148f8ccc5aef7e0c9208af104002
Srikanth Dumpeti <d.sri6365@gmail.com>
2022-10-15 01:42:52 +0530

    Find Array permutations - Approach 3

--- DSA solve/Recursion/src/StringProblems/ArrayPermutations_3.java
+++ DSA solve/Recursion/src/StringProblems/ArrayPermutations_3.java
@@ -1,29 +1,30 @@
 package StringProblems;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
-public class ArrayPermutations_2 {
+public class ArrayPermutations_3 {
 
     public static void main(String[] args) {
         ArrayList<Integer> input = new ArrayList<>();
         input.add(1);
         input.add(2);
         input.add(3);
-        System.out.println(findPermutations(input, 0, new ArrayList<ArrayList<Integer>>() ));
+        System.out.println(findPermutations(input, 0 ));
     }
-    public static ArrayList<ArrayList<Integer>> findPermutations(ArrayList<Integer> a, int pos, ArrayList<ArrayList<Integer>> output){
+    public static ArrayList<ArrayList<Integer>> findPermutations(ArrayList<Integer> a, int pos){
         if(pos == a.size() - 1){
             ArrayList<Integer> list = new ArrayList<>();
             list.addAll(a);
+            ArrayList<ArrayList<Integer>> output = new ArrayList<ArrayList<Integer>>();
             output.add(list);
             return output;
         }
+        ArrayList<ArrayList<Integer>> output = new ArrayList<ArrayList<Integer>>();
         for(int i=pos;i<a.size();i++){
                 Collections.swap(a, i, pos);
                 //System.out.println(a);
-                findPermutations(a,pos + 1, output);
+                output.addAll(findPermutations(a,pos + 1));
                 Collections.swap(a, i, pos);
         }
         return output;
